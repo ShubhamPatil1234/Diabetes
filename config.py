44543ca1cf0620e89190b5b3642eac559ca24fc3
Shubham Patil <shubham77patil@gmail.com>
2022-12-16 19:49:26 +0530

    Config file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 5505
+
+MODEL_FILE_PATH = os.path.join("models", "dt_clf.pkl")
+
+JSON_FILE_PATH = os.path.join("models", "project_data.json")
